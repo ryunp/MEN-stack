07d1725a449675dce0e24854a6570e169616e182
Ryan Paul <ryunpaul@gmail.com>
2017-07-24 15:52:43 -0700

    added express-session module

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// sessions
+app.use(session({
+  secret: 'everydaybro',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
